dd360b13c823893a8050a3a4124536e143dedb96
Iceman8911 <wuchijss3@gmail.com>
2023-05-16 16:43:27 +0100

    Update pokedex_plus_hgss.c

--- src/pokedex_plus_hgss.c
+++ src/pokedex_plus_hgss.c
@@ -142,7 +142,7 @@ static const u8 sText_TenDashes2[] = _("----------");
 
 #define SCROLLING_MON_X 146
 #define HGSS_DECAPPED 1 //0 false, 1 true
-#define HGSS_DARK_MODE 0 //0 false, 1 true
+#define HGSS_DARK_MODE 0 //0 false, 1 true    Uhh, the palettes for dark mode are actually the same with the default mode
 #define HGSS_HIDE_UNSEEN_EVOLUTION_NAMES 0 //0 false, 1 true
 
 
