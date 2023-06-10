0e2289c03c3bb9cba5c9103620eef485094b1346
PCG06 <prajithgomes06@gmail.com>
2023-06-10 17:00:19 +0530

    Changed some bag counts

--- src/new_game.c
+++ src/new_game.c
@@ -91,9 +91,9 @@ static void InitPlayerTrainerId(void)
 static void SetDefaultOptions(void)
 {
     gSaveBlock2Ptr->optionsTextSpeed = OPTIONS_TEXT_SPEED_FAST;
-    gSaveBlock2Ptr->optionsWindowFrameType = 3;
+    gSaveBlock2Ptr->optionsWindowFrameType = 2;
     gSaveBlock2Ptr->optionsSound = OPTIONS_SOUND_MONO;
-    gSaveBlock2Ptr->optionsBattleStyle = OPTIONS_BATTLE_STYLE_SET;
+    gSaveBlock2Ptr->optionsBattleStyle = OPTIONS_BATTLE_STYLE_SHIFT;
     gSaveBlock2Ptr->optionsBattleSceneOff = FALSE;
     gSaveBlock2Ptr->regionMapZoom = FALSE;
 }
