bdf2446c3e4a0413b1b0be08a325c3da1a29fb2a
PCG06 <prajithgomes06@gmail.com>
2023-08-05 18:09:46 +0530

    Fix

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -2802,7 +2802,7 @@ static void ResetWindows(void)
 
 static void PrintTextOnWindow(u8 windowId, const u8 *string, u8 x, u8 y, u8 lineSpacing, u8 colorId)
 {
-    AddTextPrinterParameterized4(windowId, FONT_TINY_NARROW, x, y, 0, lineSpacing, sTextColors[colorId], 0, string);
+    AddTextPrinterParameterized4(windowId, FONT_NORMAL, x, y, 0, lineSpacing, sTextColors[colorId], 0, string);
 }
 
 static void PrintMonInfo(void)
