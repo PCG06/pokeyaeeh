55ed45bc1a1265bb1f02ea5f8bfa7da2c8d214e2
PCG06 <pcggaming06@gmail.com>
2023-09-12 19:16:14 +0530

    Manually implemented Ghoulslash's overworld expansion
    https://github.com/ghoulslash/pokeemerald/tree/overworld-expansion

--- include/constants/event_objects.h
+++ include/constants/event_objects.h
@@ -252,7 +252,7 @@
 // object graphics that can be removed. If more graphics are needed, anything that
 // stores graphics ids will need to be increased in size. See wiki entry below:
 // https://github.com/pret/pokeemerald/wiki/Feature-Branches#overworld-expansion
-#define NUM_OBJ_EVENT_GFX                        239
+#define NUM_OBJ_EVENT_GFX                        250
 
 
 // These are dynamic object gfx ids.
