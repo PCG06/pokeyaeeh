7d895cf7710c61fb38a894c3391ef769b91c4547
PCG06 <pcggaming06@gmail.com>
2023-10-18 21:23:09 +0530

    No items in battle against boss battles

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1310,7 +1310,7 @@
 #define FLAG_UNUSED_0x4EC                                           0x4EC // Unused Flag
 #define FLAG_UNUSED_0x4ED                                           0x4ED // Unused Flag
 #define FLAG_UNUSED_0x4EE                                           0x4EE // Unused Flag
-#define FLAG_UNUSED_0x4EF                                           0x4EF // Unused Flag
+#define FLAG_NO_BAG_USE                                             0x4EF // Unused Flag
 
 #define FLAG_DEFEATED_RUSTBORO_GYM                                  0x4F0
 #define FLAG_DEFEATED_DEWFORD_GYM                                   0x4F1
