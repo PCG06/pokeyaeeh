e217b7ce850282ba3e84666e7fe74d86f96fe12b
PCG06 <pcggaming06@gmail.com>
2023-10-20 22:52:30 +0530

    Enable terrians in battles with a variable

--- include/config/battle.h
+++ include/config/battle.h
@@ -166,7 +166,7 @@
 // Var Settings
 // To use the following features in scripting, replace the 0s with the var ID you're assigning it to.
 // Eg: Replace with VAR_UNUSED_0x40F7 so you can use VAR_TERRAIN for that feature.
-#define VAR_TERRAIN                 0     // If this var has a value, assigning a STATUS_FIELD_xx_TERRAIN to it before battle causes the battle to start with that terrain active
+#define VAR_TERRAIN                 VAR_TERRAIN_SET            // If this var has a value, assigning a STATUS_FIELD_xx_TERRAIN to it before battle causes the battle to start with that terrain active
 #define B_VAR_WILD_AI_FLAGS         0     // If not 0, you can use this var to add to default wild AI flags. NOT usable with flags above (1 << 15)
 
 // Flag and Var settings
