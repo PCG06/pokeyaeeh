b9f2385f0b35090c32291c1abe7bb11446e45ee4
hedara90 <90hedara@gmail.com>
2023-11-10 01:57:04 +0100

    Added Teal Mask item graphics + updated install instructions (#3527)
    Co-authored-by: Eduardo Quezada D'Ottone <eduardo602002@gmail.com>

--- include/graphics.h
+++ include/graphics.h
@@ -9060,6 +9060,12 @@ extern const u32 gItemIcon_LinkingCord[];
 extern const u32 gItemIconPalette_LinkingCord[];
 extern const u32 gItemIcon_PeatBlock[];
 extern const u32 gItemIconPalette_PeatBlock[];
+extern const u32 gItemIcon_SyrupyApple[];
+extern const u32 gItemIconPalette_SyrupyApple[];
+extern const u32 gItemIcon_UnremarkableTeacup[];
+extern const u32 gItemIconPalette_UnremarkableTeacup[];
+extern const u32 gItemIcon_MasterpieceTeacup[];
+extern const u32 gItemIconPalette_MasterpieceTeacup[];
 // Nectars
 extern const u32 gItemIcon_RedNectar[];
 extern const u32 gItemIconPalette_RedNectar[];
@@ -9772,6 +9778,8 @@ extern const u32 gItemIcon_CatchingCharm[];
 extern const u32 gItemIconPalette_CatchingCharm[];
 extern const u32 gItemIcon_ExpCharm[];
 extern const u32 gItemIconPalette_ExpCharm[];
+extern const u32 gItemIcon_GlimmeringCharm[];
+extern const u32 gItemIconPalette_GlimmeringCharm[];
 // Form-changing Key Items
 extern const u32 gItemIcon_RotomCatalog[];
 extern const u32 gItemIconPalette_RotomCatalog[];
@@ -9963,6 +9971,22 @@ extern const u32 gItemIconPalette_BerserkGene[];
 
 extern const u32 gItemIcon_FairyFeather[];
 extern const u32 gItemIconPalette_FairyFeather[];
+
+extern const u32 gItemIcon_Mochi[];
+extern const u32 gItemIconPalette_HealthMochi[];
+extern const u32 gItemIconPalette_MuscleMochi[];
+extern const u32 gItemIconPalette_ResistMochi[];
+extern const u32 gItemIconPalette_GeniusMochi[];
+extern const u32 gItemIconPalette_CleverMochi[];
+extern const u32 gItemIconPalette_SwiftMochi[];
+extern const u32 gItemIconPalette_FreshStartMochi[];
+
+extern const u32 gItemIcon_WellspringMask[];
+extern const u32 gItemIconPalette_WellspringMask[];
+extern const u32 gItemIcon_HearthflameMask[];
+extern const u32 gItemIconPalette_HearthflameMask[];
+extern const u32 gItemIcon_CornerstoneMask[];
+extern const u32 gItemIconPalette_CornerstoneMask[];
 
 extern const u32 gItemIcon_ReturnToFieldArrow[];
 extern const u32 gItemIconPalette_ReturnToFieldArrow[];
