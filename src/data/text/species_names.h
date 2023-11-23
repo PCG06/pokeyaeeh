pret/pokeemerald

Try to better abbreviate >10 character names

src/data/text/species_names.h
@@ -948,10 +948,10 @@ const u8 gSpeciesNames[NUM_SPECIES][POKEMON_NAME_LENGTH + 1] = {
     [SPECIES_SMOLIV] = _("Smoliv"),
     [SPECIES_DOLLIV] = _("Dolliv"),
     [SPECIES_ARBOLIVA] = _("Arboliva"),
-    [SPECIES_SQUAWKABILLY_GREEN_PLUMAGE] = _("Squawkabil"),
-    [SPECIES_SQUAWKABILLY_BLUE_PLUMAGE] = _("Squawkabil"),
-    [SPECIES_SQUAWKABILLY_YELLOW_PLUMAGE] = _("Squawkabil"),
-    [SPECIES_SQUAWKABILLY_WHITE_PLUMAGE] = _("Squawkabil"),
+    [SPECIES_SQUAWKABILLY_GREEN_PLUMAGE] = _("Sqawkabily"),
+    [SPECIES_SQUAWKABILLY_BLUE_PLUMAGE] = _("Sqawkabily"),
+    [SPECIES_SQUAWKABILLY_YELLOW_PLUMAGE] = _("Sqawkabily"),
+    [SPECIES_SQUAWKABILLY_WHITE_PLUMAGE] = _("Sqawkabily"),
     [SPECIES_NACLI] = _("Nacli"),
     [SPECIES_NACLSTACK] = _("Naclstack"),
     [SPECIES_GARGANACL] = _("Garganacl"),
@@ -961,13 +961,13 @@ const u8 gSpeciesNames[NUM_SPECIES][POKEMON_NAME_LENGTH + 1] = {
     [SPECIES_TADBULB] = _("Tadbulb"),
     [SPECIES_BELLIBOLT] = _("Bellibolt"),
     [SPECIES_WATTREL] = _("Wattrel"),
-    [SPECIES_KILOWATTREL] = _("Kilowattre"),
+    [SPECIES_KILOWATTREL] = _("Kilowatrel"),
     [SPECIES_MASCHIFF] = _("Maschiff"),
     [SPECIES_MABOSSTIFF] = _("Mabosstiff"),
     [SPECIES_SHROODLE] = _("Shroodle"),
     [SPECIES_GRAFAIAI] = _("Grafaiai"),
     [SPECIES_BRAMBLIN] = _("Bramblin"),
-    [SPECIES_BRAMBLEGHAST] = _("Bramblegha"),
+    [SPECIES_BRAMBLEGHAST] = _("Brmblghast"),
     [SPECIES_TOEDSCOOL] = _("Toedscool"),
     [SPECIES_TOEDSCRUEL] = _("Toedscruel"),
     [SPECIES_KLAWF] = _("Klawf"),
@@ -1005,21 +1005,21 @@ const u8 gSpeciesNames[NUM_SPECIES][POKEMON_NAME_LENGTH + 1] = {
     [SPECIES_ANNIHILAPE] = _("Annihilape"),
     [SPECIES_CLODSIRE] = _("Clodsire"),
     [SPECIES_FARIGIRAF] = _("Farigiraf"),
-    [SPECIES_DUDUNSPARCE_TWO_SEGMENT] = _("Dudunsparc"),
-    [SPECIES_DUDUNSPARCE_THREE_SEGMENT] = _("Dudunsparc"),
+    [SPECIES_DUDUNSPARCE_TWO_SEGMENT] = _("Dudunsprce"),
+    [SPECIES_DUDUNSPARCE_THREE_SEGMENT] = _("Dudunsprce"),
     [SPECIES_KINGAMBIT] = _("Kingambit"),
     [SPECIES_GREAT_TUSK] = _("Great Tusk"),
-    [SPECIES_SCREAM_TAIL] = _("Scream Tai"),
-    [SPECIES_BRUTE_BONNET] = _("Brute Bonn"),
-    [SPECIES_FLUTTER_MANE] = _("Flutter Ma"),
-    [SPECIES_SLITHER_WING] = _("Slither Wi"),
-    [SPECIES_SANDY_SHOCKS] = _("Sandy Shoc"),
-    [SPECIES_IRON_TREADS] = _("Iron Tread"),
-    [SPECIES_IRON_BUNDLE] = _("Iron Bundl"),
+    [SPECIES_SCREAM_TAIL] = _("ScreamTail"),
+    [SPECIES_BRUTE_BONNET] = _("BruteBonet"),
+    [SPECIES_FLUTTER_MANE] = _("FluttrMane"),
+    [SPECIES_SLITHER_WING] = _("SlithrWing"),
+    [SPECIES_SANDY_SHOCKS] = _("SndyShocks"),
+    [SPECIES_IRON_TREADS] = _("IronTreads"),
+    [SPECIES_IRON_BUNDLE] = _("IronBundle"),
     [SPECIES_IRON_HANDS] = _("Iron Hands"),
-    [SPECIES_IRON_JUGULIS] = _("Iron Jugul"),
+    [SPECIES_IRON_JUGULIS] = _("IronJuguls"),
     [SPECIES_IRON_MOTH] = _("Iron Moth"),
-    [SPECIES_IRON_THORNS] = _("Iron Thorn"),
+    [SPECIES_IRON_THORNS] = _("IronThorns"),
     [SPECIES_FRIGIBAX] = _("Frigibax"),
     [SPECIES_ARCTIBAX] = _("Arctibax"),
     [SPECIES_BAXCALIBUR] = _("Baxcalibur"),
@@ -1030,20 +1030,20 @@ const u8 gSpeciesNames[NUM_SPECIES][POKEMON_NAME_LENGTH + 1] = {
     [SPECIES_CHIEN_PAO] = _("Chien-Pao"),
     [SPECIES_TING_LU] = _("Ting-Lu"),
     [SPECIES_CHI_YU] = _("Chi-Yu"),
-    [SPECIES_ROARING_MOON] = _("Roaring Mo"),
-    [SPECIES_IRON_VALIANT] = _("Iron Valia"),
+    [SPECIES_ROARING_MOON] = _("RoarngMoon"),
+    [SPECIES_IRON_VALIANT] = _("IronVliant"),
     [SPECIES_KORAIDON] = _("Koraidon"),
     [SPECIES_MIRAIDON] = _("Miraidon"),
-    [SPECIES_WALKING_WAKE] = _("Walking Wa"),
-    [SPECIES_IRON_LEAVES] = _("Iron Leave"),
+    [SPECIES_WALKING_WAKE] = _("WalkngWake"),
+    [SPECIES_IRON_LEAVES] = _("IronLeaves"),
     [SPECIES_DIPPLIN] = _("Dipplin"),
-    [SPECIES_POLTCHAGEIST_COUNTERFEIT] = _("Poltchagei"),
-    [SPECIES_POLTCHAGEIST_ARTISAN] = _("Poltchagei"),
+    [SPECIES_POLTCHAGEIST_COUNTERFEIT] = _("Ptchageist"),
+    [SPECIES_POLTCHAGEIST_ARTISAN] = _("Ptchageist"),
     [SPECIES_SINISTCHA_UNREMARKABLE] = _("Sinistcha"),
     [SPECIES_SINISTCHA_MASTERPIECE] = _("Sinistcha"),
     [SPECIES_OKIDOGI] = _("Okidogi"),
     [SPECIES_MUNKIDORI] = _("Munkidori"),
-    [SPECIES_FEZANDIPITI] = _("Fezandipit"),
+    [SPECIES_FEZANDIPITI] = _("Fezndipiti"),
     [SPECIES_OGERPON_TEAL_MASK] = _("Ogerpon"),
     [SPECIES_OGERPON_WELLSPRING_MASK] = _("Ogerpon"),
     [SPECIES_OGERPON_HEARTHFLAME_MASK] = _("Ogerpon"),

PCG06/pokeyaeeh

src/data/text/species_names.h
@@ -941,7 +941,7 @@ const u8 gSpeciesNames[NUM_SPECIES][POKEMON_NAME_LENGTH + 1] = {
     [SPECIES_SMOLIV] = _("Smoliv"),
     [SPECIES_DOLLIV] = _("Dolliv"),
     [SPECIES_ARBOLIVA] = _("Arboliva"),
-    [SPECIES_SQUAWKABILLY_GREEN_PLUMAGE] = _("Squawkabil"),
+    [SPECIES_SQUAWKABILLY_GREEN_PLUMAGE] = _("Sqawkabily"),
     [SPECIES_NACLI] = _("Nacli"),
     [SPECIES_NACLSTACK] = _("Naclstack"),
     [SPECIES_GARGANACL] = _("Garganacl"),
@@ -951,13 +951,13 @@ const u8 gSpeciesNames[NUM_SPECIES][POKEMON_NAME_LENGTH + 1] = {
     [SPECIES_TADBULB] = _("Tadbulb"),
     [SPECIES_BELLIBOLT] = _("Bellibolt"),
     [SPECIES_WATTREL] = _("Wattrel"),
-    [SPECIES_KILOWATTREL] = _("Kilowattre"),
+    [SPECIES_KILOWATTREL] = _("Kilowatrel"),
     [SPECIES_MASCHIFF] = _("Maschiff"),
     [SPECIES_MABOSSTIFF] = _("Mabosstiff"),
     [SPECIES_SHROODLE] = _("Shroodle"),
     [SPECIES_GRAFAIAI] = _("Grafaiai"),
     [SPECIES_BRAMBLIN] = _("Bramblin"),
-    [SPECIES_BRAMBLEGHAST] = _("Bramblegha"),
+    [SPECIES_BRAMBLEGHAST] = _("Bramblghst"),
     [SPECIES_TOEDSCOOL] = _("Toedscool"),
     [SPECIES_TOEDSCRUEL] = _("Toedscruel"),
     [SPECIES_KLAWF] = _("Klawf"),
@@ -992,20 +992,20 @@ const u8 gSpeciesNames[NUM_SPECIES][POKEMON_NAME_LENGTH + 1] = {
     [SPECIES_ANNIHILAPE] = _("Annihilape"),
     [SPECIES_CLODSIRE] = _("Clodsire"),
     [SPECIES_FARIGIRAF] = _("Farigiraf"),
-    [SPECIES_DUDUNSPARCE_TWO_SEGMENT] = _("Dudunsparc"),
+    [SPECIES_DUDUNSPARCE_TWO_SEGMENT] = _("Dudunsprce"),
     [SPECIES_KINGAMBIT] = _("Kingambit"),
     [SPECIES_GREAT_TUSK] = _("Great Tusk"),
-    [SPECIES_SCREAM_TAIL] = _("Scream Tai"),
-    [SPECIES_BRUTE_BONNET] = _("Brute Bonn"),
-    [SPECIES_FLUTTER_MANE] = _("Flutter Ma"),
-    [SPECIES_SLITHER_WING] = _("Slither Wi"),
-    [SPECIES_SANDY_SHOCKS] = _("Sandy Shoc"),
-    [SPECIES_IRON_TREADS] = _("Iron Tread"),
-    [SPECIES_IRON_BUNDLE] = _("Iron Bundl"),
+    [SPECIES_SCREAM_TAIL] = _("ScreamTail"),
+    [SPECIES_BRUTE_BONNET] = _("BruteBonet"),
+    [SPECIES_FLUTTER_MANE] = _("FluttrMane"),
+    [SPECIES_SLITHER_WING] = _("SlithrWing"),
+    [SPECIES_SANDY_SHOCKS] = _("SndyShocks"),
+    [SPECIES_IRON_TREADS] = _("IronTreads"),
+    [SPECIES_IRON_BUNDLE] = _("IronBundle"),
     [SPECIES_IRON_HANDS] = _("Iron Hands"),
-    [SPECIES_IRON_JUGULIS] = _("Iron Jugul"),
+    [SPECIES_IRON_JUGULIS] = _("IronJuguls"),
     [SPECIES_IRON_MOTH] = _("Iron Moth"),
-    [SPECIES_IRON_THORNS] = _("Iron Thorn"),
+    [SPECIES_IRON_THORNS] = _("IronThorns"),
     [SPECIES_FRIGIBAX] = _("Frigibax"),
     [SPECIES_ARCTIBAX] = _("Arctibax"),
     [SPECIES_BAXCALIBUR] = _("Baxcalibur"),
@@ -1015,12 +1015,12 @@ const u8 gSpeciesNames[NUM_SPECIES][POKEMON_NAME_LENGTH + 1] = {
     [SPECIES_CHIEN_PAO] = _("Chien-Pao"),
     [SPECIES_TING_LU] = _("Ting-Lu"),
     [SPECIES_CHI_YU] = _("Chi-Yu"),
-    [SPECIES_ROARING_MOON] = _("Roaring Mo"),
-    [SPECIES_IRON_VALIANT] = _("Iron Valia"),
+    [SPECIES_ROARING_MOON] = _("RoarngMoon"),
+    [SPECIES_IRON_VALIANT] = _("IronVliant"),
     [SPECIES_KORAIDON] = _("Koraidon"),
     [SPECIES_MIRAIDON] = _("Miraidon"),
-    [SPECIES_WALKING_WAKE] = _("Walking Wa"),
-    [SPECIES_IRON_LEAVES] = _("Iron Leave"),
+    [SPECIES_WALKING_WAKE] = _("WalkngWake"),
+    [SPECIES_IRON_LEAVES] = _("IronLeaves"),
 #endif
 
     // Megas
@@ -1489,16 +1489,16 @@ const u8 gSpeciesNames[NUM_SPECIES][POKEMON_NAME_LENGTH + 1] = {
     // Maushold
     [SPECIES_MAUSHOLD_FAMILY_OF_THREE] = _("Maushold"),
     // Squawkabilly
-    [SPECIES_SQUAWKABILLY_BLUE_PLUMAGE] = _("Squawkabil"),
-    [SPECIES_SQUAWKABILLY_YELLOW_PLUMAGE] = _("Squawkabil"),
-    [SPECIES_SQUAWKABILLY_WHITE_PLUMAGE] = _("Squawkabil"),
+    [SPECIES_SQUAWKABILLY_BLUE_PLUMAGE] = _("Sqawkabily"),
+    [SPECIES_SQUAWKABILLY_YELLOW_PLUMAGE] = _("Sqawkabily"),
+    [SPECIES_SQUAWKABILLY_WHITE_PLUMAGE] = _("Sqawkabily"),
     // Palafin
     [SPECIES_PALAFIN_HERO] = _("Palafin"),
     // Tatsugiri
     [SPECIES_TATSUGIRI_DROOPY] = _("Tatsugiri"),
     [SPECIES_TATSUGIRI_STRETCHY] = _("Tatsugiri"),
     // Dudunsparce
-    [SPECIES_DUDUNSPARCE_THREE_SEGMENT] = _("Dudunsparc"),
+    [SPECIES_DUDUNSPARCE_THREE_SEGMENT] = _("Dudunsprce"),
     // Gimmighoul
     [SPECIES_GIMMIGHOUL_ROAMING] = _("Gimmighoul"),
 #endif