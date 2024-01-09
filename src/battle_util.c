2565b4cc4dd743a5b91912329e2e1a00071f5f1c
PCG06 <pcggaming06@gmail.com>
2024-01-09 20:08:26 +0530

    Normalize buff I totally didn't forget about!

--- src/battle_util.c
+++ src/battle_util.c
@@ -10169,7 +10169,7 @@ static inline void MulByTypeEffectiveness(uq4_12_t *modifier, u32 move, u32 move
         mod = UQ_4_12(1.0);
     }
     else if ((moveType == TYPE_FIGHTING || moveType == TYPE_NORMAL) && defType == TYPE_GHOST
-        && (abilityAtk == ABILITY_SCRAPPY || abilityAtk == ABILITY_MINDS_EYE)
+        && (abilityAtk == ABILITY_SCRAPPY || abilityAtk == ABILITY_NORMALIZE || abilityAtk == ABILITY_MINDS_EYE)
         && mod == UQ_4_12(0.0))
     {
         mod = UQ_4_12(1.0);
