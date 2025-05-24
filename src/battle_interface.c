d47bad6af3cbcc19a4c90dcf1f0cbeecefd9db41
PCG06 <pcggaming06@gmail.com>
2025-05-24 23:34:06 +0530

    Update health boxes

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -373,7 +373,7 @@ static const struct Subsprite sHealthBar_Subsprites_Player[] =
 static const struct Subsprite sHealthBar_Subsprites_Opponent[] =
 {
     {
-        .x = -16,
+        .x = -17,
         .y = 0,
         .shape = SPRITE_SHAPE(32x8),
         .size = SPRITE_SIZE(32x8),
@@ -381,7 +381,7 @@ static const struct Subsprite sHealthBar_Subsprites_Opponent[] =
         .priority = 1
     },
     {
-        .x = 16,
+        .x = 15,
         .y = 0,
         .shape = SPRITE_SHAPE(32x8),
         .size = SPRITE_SIZE(32x8),
